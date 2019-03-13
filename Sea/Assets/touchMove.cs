d036618245ae8d0a5af62398d0439b2fd5f277a8
Maksim <thesoules2000@gmail.com>
2019-03-13 20:59:37 +0300

    Test

---
+++ Sea/Assets/touchMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchMove : MonoBehaviour
+{
+    Vector2 moveDirection = Vector2.zero;
+    //void Start()
+    //{
+    //    rb = GetComponent<Rigidbody2D>();
+    //}
+
+    void Update()
+    {
+        if (Input.touchCount>0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 positionChange = touch.deltaPosition;
+                positionChange.y = -positionChange.y;
+                moveDirection = positionChange.normalized;
+            }
+        }
+        transform.position += (Vector3)moveDirection * -10f * Time.deltaTime;
+    }
+   
+}
